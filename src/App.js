2a63dedd6b5d3eaeaac548dc8ba7d421d0d57563
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-13 19:37:16 +0100

    adding new files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  return (
+    <>
+      <section>
+        <Home />
+        <Modal />
+        <Sidebar />
+      </section>
+    </>
+  )
+}
+
+export default App
